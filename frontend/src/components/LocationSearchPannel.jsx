415186b74ad0603ddaaca77bc138591209453d72
sabihkahn <whyonlyhead@gmail.com>
2025-08-15 17:49:02 +0500

    added vehicle selection pannel

--- frontend/src/components/LocationSearchPannel.jsx
+++ frontend/src/components/LocationSearchPannel.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
-const LocationSearchPannel = () => {
+const LocationSearchPannel = (props) => {
+    const locations = [
+        '24b nears kappor caffe',
+        'kamil road gugrat',
+        'mda suraj mianui',
+        'fake location 3'
+    ]
+
     return (
         <div>
-            <div className='flex items-center my-4 justify-start gap-4'>
-                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-2xl'><i className="ri-map-pin-fill text-xl"></i></h2>
-                <h3>24b Nears kapoor caffe , Sheryains coading school</h3>
-            </div>
-
-            <div className='flex items-center my-4 justify-start gap-4'>
-                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-2xl'><i className="ri-map-pin-fill text-xl"></i></h2>
-                <h3>24b Nears kapoor caffe , Sheryains coading school</h3>
-            </div>
-
-            <div className='flex items-center my-4 justify-start gap-4'>
-                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-2xl'><i className="ri-map-pin-fill text-xl"></i></h2>
-                <h3>24b Nears kapoor caffe , Sheryains coading school</h3>
-            </div>
-
-
-
+            {
+                locations.map((e, idx) => {
+                    return <div key={idx} onClick={()=>{
+                        props.setvehiclepannelOpen(true) 
+                        props.setpanelOpen(false)
+                    }} className='flex border-2 p-3 rounded-xl border-gray-300 active:border-black items-center my-4 justify-start gap-4'>
+                        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-2xl'><i className="ri-map-pin-fill text-xl"></i></h2>
+                        <h3>{e}</h3>
+                    </div>
+                })
+            }
         </div>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,14 +3,19 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPannel from '../components/LocationSearchPannel'
+import VehiclePannel from '../components/VehiclePannel'
+import ConfirmRide from '../components/ConfirmRide'
 const Home = () => {
 
   const [pick, setpick] = useState('')
   const [destination, setdestination] = useState('')
   const [panelOpen, setpanelOpen] = useState(false)
   const pannelcloseref = useRef(null)
+  const [vehiclepannelOpen, setvehiclepannelOpen] = useState(false)
   const panelRef = useRef(null)
-
+  const vehiclepannelref = useRef(null)
+  const [confirmRidepannel, setconfirmRidepannel] = useState(false)
+  const confirmRidepannelref = useRef(null)
   const submitHandler = async (e) => {
     e.preventDefault()
   }
@@ -20,7 +25,10 @@ const Home = () => {
       gsap.to(panelRef.current,
         {
           height: '70%',
-          padding:24
+          padding: 24,
+          duration: 0.4,
+          ease: 'power2.out'
+
         })
       gsap.to(pannelcloseref.current, {
         opacity: '1'
@@ -28,7 +36,9 @@ const Home = () => {
     } else {
       gsap.to(panelRef.current, {
         height: '0%',
-        padding:0
+        padding: 0,
+        duration: 0.3,
+        ease: 'power2.in'
       })
       gsap.to(pannelcloseref.current, {
         opacity: '0'
@@ -37,10 +47,33 @@ const Home = () => {
   }, [panelOpen])
 
 
+  useGSAP(() => {
+    if (vehiclepannelOpen) {
+      gsap.to(vehiclepannelref.current, {
+        transform: 'translateY(0)'
+      })
+    }else{
+      gsap.to(vehiclepannelref.current,{
+        transform:"translateY(100%)"
+      })
+    }
+  },[vehiclepannelOpen])
+ 
+    useGSAP(() => {
+    if (confirmRidepannel) {
+      gsap.to(confirmRidepannelref.current, {
+        transform: 'translateY(0)'
+      })
+    }else{
+      gsap.to(confirmRidepannelref.current,{
+        transform:"translateY(100%)"
+      })
+    }
+  },[confirmRidepannel])
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
       <div className='h-screen w-screen'>
         <img className='h-full w-full object-cover' src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/w_1313,h_1313,c_limit/GoogleMapTA.jpg" alt="" />
@@ -69,9 +102,15 @@ const Home = () => {
               value={destination} />
           </form>
         </div>
-        <div ref={panelRef} className='h-[0%] bg-white   '>
-          <LocationSearchPannel />
+        <div ref={panelRef} className='h-[0%] bg-white overflow-hidden  '>
+          <LocationSearchPannel vehiclepannelOpen={vehiclepannelOpen} setvehiclepannelOpen={setvehiclepannelOpen} panelOpen={panelOpen} setpanelOpen={setpanelOpen} />
         </div>
+      </div>
+      <div ref={vehiclepannelref} className='fixed z-10 bottom-0 w-full  bg-white px-3 py-6 translate-y-full'>
+      <VehiclePannel setvehiclepannelOpen={setvehiclepannelOpen} />
+      </div>
+      <div ref={confirmRidepannelref} className='fixed z-10 bottom-0 w-full  bg-white px-3 py-6 translate-y-full'>
+      <ConfirmRide />
       </div>
     </div>
   )
